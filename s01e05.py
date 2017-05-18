6c717f49b00d19cbd76348183c917bd2c3efb673
Michal Fularz <michal.fularz@gmail.com>
2017-05-18 23:33:55 +0200

    Lab 5 work in progress + files used (haar cascades from OpenCV).

---
+++ s01e05.py
@@ -0,0 +1,145 @@
+import numpy as np
+import cv2
+
+
+def ex_0():
+    img = cv2.imread("_data/no_idea.jpg", cv2.IMREAD_GRAYSCALE)
+
+    # Initiate FAST object with default values
+    fast = cv2.FastFeatureDetector_create(threshold=100,
+                                          nonmaxSuppression=True
+                                          )
+
+    # find and draw the keypoints
+    kp = fast.detect(img)
+    img2 = cv2.drawKeypoints(img, kp, img, color=(255, 0, 0))
+
+    cv2.imshow("fast", img2)
+    cv2.waitKey(0)
+
+
+def ex_1():
+    img1 = cv2.imread("_data/no_idea.jpg", cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread("_data/no_idea.jpg", cv2.IMREAD_GRAYSCALE)
+
+    cap = cv2.VideoCapture(0)
+
+    key = ord('a')
+    while key != ord('q'):
+        _, frame = cap.read()
+        cv2.imshow("img", frame)
+        key = cv2.waitKey(50)
+
+    img1 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    key = ord('a')
+    while key != ord('q'):
+        _, frame = cap.read()
+        cv2.imshow("img", frame)
+        key = cv2.waitKey(50)
+
+    img2 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    orb = cv2.ORB_create()
+    detector = cv2.AKAZE_create()
+    #detector = cv2.FastFeatureDetector_create()
+
+    # find the keypoints and descriptors with SIFT
+
+    kp1 = detector.detect(img1, None)
+    kp2 = detector.detect(img2, None)
+
+    _, des1 = orb.compute(img1, kp1)
+    _, des2 = orb.compute(img2, kp2)
+
+    #kp1, des1 = orb.detectAndCompute(img1, None)
+    #kp2, des2 = orb.detectAndCompute(img2, None)
+
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors.
+    matches = bf.match(des1, des2)
+
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key=lambda x: x.distance)
+
+    # Draw first 10 matches.
+    img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:], None)
+
+    cv2.imshow("matches", img3)
+    cv2.waitKey(0)
+
+
+def ex_2():
+    face_cascade = cv2.CascadeClassifier("_data/s01e05/haarcascade_frontalface_default.xml")
+    eye_cascade = cv2.CascadeClassifier("_data/s01e05/haarcascade_eye.xml")
+
+    cap = cv2.VideoCapture(0)
+
+    key = ord('a')
+    while key != ord('q'):
+        _, img = cap.read()
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            roi_gray = gray[y:y + h, x:x + w]
+            roi_color = img[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        cv2.imshow("img", img)
+        key = cv2.waitKey(50)
+
+    cv2.destroyAllWindows()
+
+
+def ex_3():
+    def nothing(x):
+        pass
+
+    cap = cv2.VideoCapture(0)
+
+    cv2.namedWindow('img')
+    cv2.createTrackbar('low', 'img', 200, 255, nothing)
+    cv2.createTrackbar('high', 'img', 50, 255, nothing)
+
+    key = ord('a')
+    while key != ord('q'):
+        _, img = cap.read()
+
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+        img_h, img_s, img_v = cv2.split(img_hsv)
+
+        img_analysed = img_s
+
+        threshold_low = cv2.getTrackbarPos('low', 'img')
+        threshold_high = cv2.getTrackbarPos('high', 'img')
+
+        _, img_after_low = cv2.threshold(img_analysed, threshold_low, 255, cv2.THRESH_BINARY_INV)
+        _, img_after_high = cv2.threshold(img_analysed, threshold_high, 255, cv2.THRESH_BINARY)
+
+        img_after_both = cv2.bitwise_and(img_after_low, img_after_high)
+
+        img_in_range = cv2.inRange(img_hsv, (0, 140, 160), (255, 190, 200))
+
+        res = cv2.bitwise_and(img, img, mask=img_in_range)
+
+        cv2.imshow('img', img_analysed)
+        cv2.imshow('img_after_low', img_after_low)
+        cv2.imshow('img_after_high', img_after_high)
+        cv2.imshow('img_after_both', img_after_both)
+        cv2.imshow("in_range", res)
+        key = cv2.waitKey(50)
+
+if __name__ == "__main__":
+    ex_0()
+    #ex_1()
+    #ex_2()
+    #ex_3()
