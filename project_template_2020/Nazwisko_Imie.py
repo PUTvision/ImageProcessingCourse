3230ad1eac2936449f9650c19975e4fbaf4890b5
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2020-05-14 19:24:30 +0200

    Fix invalid arguments and import in template

--- project_template_2020/Nazwisko_Imie.py
+++ project_template_2020/Nazwisko_Imie.py
@@ -4,19 +4,19 @@ from pathlib import Path
 
 import cv2
 
-from .processing.utils import perform_processing
+from processing.utils import perform_processing
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('images-dir', type=str)
-    parser.add_argument('results-file', type=str)
+    parser.add_argument('images_dir', type=str)
+    parser.add_argument('results_file', type=str)
     args = parser.parse_args()
 
-    images_dir = Path(args['images-dir'])
-    results_file = Path(args['results-file'])
+    images_dir = Path(args.images_dir)
+    results_file = Path(args.results_file)
 
-    images_paths = [image_path for image_path in images_dir.iterdir() if image_path.name.endswith('.jpg')]
+    images_paths = sorted([image_path for image_path in images_dir.iterdir() if image_path.name.endswith('.jpg')])
     results = {}
     for image_path in images_paths:
         image = cv2.imread(str(image_path))
